3ecce6dc2ddefd8de332f7c658412d67c627c929
Randy Tolentino <makertolentino@gmail.com>
2015-02-07 07:03:23 -0600

    smooth slide in fadein for sections

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
 	$('#about').click(function(){
+		$(this).toggleClass('animated fadeInRight');
 		$('.about').fadeToggle();
 		$('#official-logo').fadeToggle();
-		$('#work').fadeToggle();
-		$('#contact').fadeToggle();
+		$('#work').fadeToggle('fast');
+		$('#contact').fadeToggle('fast');
 	});
 	$('#work').click(function(){
+		$(this).toggleClass('animated fadeInRight');
 		$('.work').fadeToggle();
 		$('#official-logo').fadeToggle();
-		$('#about').fadeToggle();
-		$('#contact').fadeToggle();
+		$('#about').fadeToggle('fast');
+		$('#contact').fadeToggle('fast');
 	});
 	$('#contact').click(function(){
+		$(this).toggleClass('animated fadeInRight');
 		$('.contact').fadeToggle();
 		$('#official-logo').fadeToggle();
-		$('#about').fadeToggle();
-		$('#work').fadeToggle();
+		$('#about').fadeToggle('fast');
+		$('#work').fadeToggle('fast');
 	});
 });
