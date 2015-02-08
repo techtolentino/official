83b0d85fe8ee78bd01bc5d2a1b1ef9a4ad379bbc
Randy Tolentino <makertolentino@gmail.com>
2015-02-08 07:04:35 -0600

    working on jquery fadeToggle speeds

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 	$('#about').click(function(){
 		$(this).toggleClass('animated fadeInRight');
-		$('.about').fadeToggle();
-		// $('#official-logo').fadeToggle(200);
+		$('.about').fadeToggle('fast');
+		$('#official-logo').toggleClass('logofy', 1000);
 		$('#work').fadeToggle('fast');
 		$('#contact').fadeToggle('fast');
 	});
 	$('#work').click(function(){
 		$(this).toggleClass('animated fadeInRight');
-		$('.work').fadeToggle();
-		// $('#official-logo').fadeToggle(200);
+		$('.work').fadeToggle('fast');
+		$('#official-logo').toggleClass('logofy', 1000);
 		$('#about').fadeToggle('fast');
 		$('#contact').fadeToggle('fast');
 	});
 	$('#contact').click(function(){
 		$(this).toggleClass('animated fadeInRight');
-		$('.contact').fadeToggle();
-		// $('#official-logo').fadeToggle(200);
+		$('.contact').fadeToggle('fast');
+		$('#official-logo').toggleClass('logofy', 1000);
 		$('#about').fadeToggle('fast');
 		$('#work').fadeToggle('fast');
 	});
