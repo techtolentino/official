989e9c14982ddb058528925de211a7fdbc760e77
Randy Tolentino <makertolentino@gmail.com>
2015-02-08 06:11:51 -0600

    animated sections

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 	$('#about').click(function(){
-		$(this).toggleClass('animated fadeInRight');
+		$(this).toggleClass('animated fadeInDown');
 		$('.about').fadeToggle();
 		$('#official-logo').fadeToggle();
 		$('#work').fadeToggle('fast');
 		$('#contact').fadeToggle('fast');
 	});
 	$('#work').click(function(){
-		$(this).toggleClass('animated fadeInRight');
+		$(this).toggleClass('animated fadeInDown');
 		$('.work').fadeToggle();
 		$('#official-logo').fadeToggle();
 		$('#about').fadeToggle('fast');
 		$('#contact').fadeToggle('fast');
 	});
 	$('#contact').click(function(){
-		$(this).toggleClass('animated fadeInRight');
+		$(this).toggleClass('animated fadeInDown');
 		$('.contact').fadeToggle();
 		$('#official-logo').fadeToggle();
 		$('#about').fadeToggle('fast');
