59a59a13dbfad10e9080126a93a19559df7d13cc
Randy Tolentino <makertolentino@gmail.com>
2015-02-08 06:57:47 -0600

    repositioning and mobile fixing

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 	$('#about').click(function(){
-		$(this).toggleClass('animated fadeInDown');
+		$(this).toggleClass('animated fadeInRight');
 		$('.about').fadeToggle();
-		$('#official-logo').fadeToggle();
+		// $('#official-logo').fadeToggle(200);
 		$('#work').fadeToggle('fast');
 		$('#contact').fadeToggle('fast');
 	});
 	$('#work').click(function(){
-		$(this).toggleClass('animated fadeInDown');
+		$(this).toggleClass('animated fadeInRight');
 		$('.work').fadeToggle();
-		$('#official-logo').fadeToggle();
+		// $('#official-logo').fadeToggle(200);
 		$('#about').fadeToggle('fast');
 		$('#contact').fadeToggle('fast');
 	});
 	$('#contact').click(function(){
-		$(this).toggleClass('animated fadeInDown');
+		$(this).toggleClass('animated fadeInRight');
 		$('.contact').fadeToggle();
-		$('#official-logo').fadeToggle();
+		// $('#official-logo').fadeToggle(200);
 		$('#about').fadeToggle('fast');
 		$('#work').fadeToggle('fast');
 	});
