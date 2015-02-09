84ba3e2429eeee8bcf5268397b125bafcfe0ff41
Randy Tolentino <makertolentino@gmail.com>
2015-02-09 06:50:37 -0600

    change on click funtions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
 	$('#about').click(function(){
 		$(this).toggleClass('animated flip');
-		$('#official-logo').toggleClass('logofy', 400);
-		$('#work').fadeToggle('fast');
-		$('#contact').fadeToggle('fast');
 		$('.about').fadeToggle('fast');
+		$('#official-logo').toggleClass('logofy', 400);
 	});
 	$('#work').click(function(){
 		$(this).toggleClass('animated flip');
-		$('#official-logo').toggleClass('logofy', 400);
-		$('#about').fadeToggle('fast');
-		$('#contact').fadeToggle('fast');
 		$('.work').fadeToggle('fast');
+		$('#official-logo').toggleClass('logofy', 400);
 	});
 	$('#contact').click(function(){
 		$(this).toggleClass('animated flip');
-		$('#official-logo').toggleClass('logofy', 400);
-		$('#about').fadeToggle('fast');
-		$('#work').fadeToggle();
 		$('.contact').fadeToggle('fast');
+		$('#official-logo').toggleClass('logofy', 400);
 	});
 });
